3701aa4ac1d80b9b0c490c81d60e81b2d7ce904c
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2019-10-10 22:42:01 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-//import the react and react dom libraries 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+//import the react and react dom libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
 //Create a react component
-
+const App = () => {
+  return <div>Hi there..</div>;
+};
 
 //Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
