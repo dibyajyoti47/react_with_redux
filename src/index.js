47bbf99ca48c4ef017250eef4c2429b0b7e90b4c
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2019-10-13 22:12:24 +0530

    section 2 cmpl

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText1(){
+  return 'Click ';
+}
 //Create a react component
 const App = () => {
-  return <div>Hi there..</div>;
+  const buttonText2 = 'Me!';
+  //const buttonText2 = {text: "Me!"};
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor:'blue',color:'white'}}>{getButtonText1()+buttonText2}</button>
+      {/* <button style={{backgroundColor:'blue',color:'white'}}>{buttonText2}</button> */}
+    </div>
+  );
 };
 
 //Take the react component and show it on the screen
