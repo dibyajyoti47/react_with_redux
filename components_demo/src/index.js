f33cdebd08d8a49b564fbdb92070ba8979b09801
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2019-10-15 21:28:39 +0530

    lession 38 compl

--- components_demo/src/index.js
+++ components_demo/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommonDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommonDetail 
                 author={faker.name.firstName()} 
                 comment={faker.lorem.sentence()} 
