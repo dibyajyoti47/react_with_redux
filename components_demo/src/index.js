27c637b4811737804cd921a23c056e5581d3b108
Dibyajyoti Behera <dibyajyoti_behera@dellteam.com>
2019-10-15 16:31:46 +0530

    lession 37 completed

--- components_demo/src/index.js
+++ components_demo/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommonDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommonDetail />
+            <CommonDetail 
+                author={faker.name.firstName()} 
+                comment={faker.lorem.sentence()} 
+                timeAgo={faker.date.recent().toDateString()} 
+                imageSrc={faker.image.avatar()}/>
+            <CommonDetail 
+                author={faker.name.firstName()} 
+                comment={faker.lorem.sentence()} 
+                timeAgo={faker.date.recent().toDateString()} 
+                imageSrc={faker.image.avatar()}/>
+            <CommonDetail 
+                author={faker.name.firstName()} 
+                comment={faker.lorem.sentence()} 
+                timeAgo={faker.date.recent().toDateString()} 
+                imageSrc={faker.image.avatar()}/>
         </div>
     );
 };
