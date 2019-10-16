4154cfaf9563110cbb9300552d6a4a584edf8811
Dibyajyoti Behera <dibyajyoti_behera@dellteam.com>
2019-10-16 12:25:25 +0530

    lession 44 completed..

--- components_demo/src/index.js
+++ components_demo/src/index.js
@@ -7,24 +7,39 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommonDetail 
-                author={faker.name.firstName()} 
-                comment={faker.lorem.sentence()} 
-                timeAgo={faker.date.recent().toDateString()} 
-                imageSrc={faker.image.avatar()}/>
-            <CommonDetail 
-                author={faker.name.firstName()} 
-                comment={faker.lorem.sentence()} 
-                timeAgo={faker.date.recent().toDateString()} 
-                imageSrc={faker.image.avatar()}/>
-            <CommonDetail 
-                author={faker.name.firstName()} 
-                comment={faker.lorem.sentence()} 
-                timeAgo={faker.date.recent().toDateString()} 
-                imageSrc={faker.image.avatar()}/>
+            <ApprovalCard>
+                Are you sure you want to do this ?
+            </ApprovalCard>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning !</h4>
+                    Are you sure you want to do this ?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommonDetail
+                    author={faker.name.firstName()}
+                    comment={faker.lorem.sentence()}
+                    timeAgo={faker.date.recent().toDateString()}
+                    imageSrc={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommonDetail
+                    author={faker.name.firstName()}
+                    comment={faker.lorem.sentence()}
+                    timeAgo={faker.date.recent().toDateString()}
+                    imageSrc={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommonDetail
+                    author={faker.name.firstName()}
+                    comment={faker.lorem.sentence()}
+                    timeAgo={faker.date.recent().toDateString()}
+                    imageSrc={faker.image.avatar()} />
+            </ApprovalCard>
+            
         </div>
     );
 };
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
