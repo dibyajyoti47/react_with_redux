2e88badca80c5aacf7886b58091873150729e562
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2019-12-04 21:45:35 +0530

    section 14 compl

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,17 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder'
 
-export const fetchPosts = async () => {
-    // Bad Approach
-    const response = await jsonPlaceHolder.get('./posts');
-    return {
-        type: 'FETCH_POSTS',
-        payLoad: response
-    };
-}
+/*  //just shortened syntax below, everything is same
+export const fetchPosts = () => {
+    return async (dispatch) => {
+        const res = await jsonPlaceHolder.get('./posts');
+        dispatch ({
+            type: 'FETCH_POSTS',
+            payLoad: res
+        });
+    };    
+} */
+
+export const fetchPosts = () => async dispatch => {
+        const res = await jsonPlaceHolder.get('./posts');
+        dispatch ({type: 'FETCH_POSTS',payLoad: res});
+    }    
