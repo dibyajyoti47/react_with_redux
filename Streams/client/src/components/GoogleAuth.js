baf7c8123be94a00ecae833314421d1dbcce0436
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2019-12-09 21:26:04 +0530

    lecture 210 compl

---
+++ Streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    componentDidMount () {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '167560716074-e37jbrqcjt648ermla1g35cp91v3l9lq.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
+    }
+    render () {
+        return <div>Google Auth</div>;
+    }
+}
+
+export default GoogleAuth;
