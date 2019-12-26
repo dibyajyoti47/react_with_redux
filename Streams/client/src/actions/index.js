6e7048ce697fca73c4f41c0aebe32f38b47b5b0c
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2019-12-27 00:21:33 +0530

    152 incompl

--- Streams/client/src/actions/index.js
+++ Streams/client/src/actions/index.js
@@ -24,15 +24,14 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async (dispatch, getState) => {
-  const { userId } = getState().auth;
-  const response = await streams.post("/streams", formValues);
-
+  const {userId} = getState().auth;
+  const response = await streams.post("/streams", {...formValues, userId});
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
 export const fetchStreams = () => async dispatch => {
     const response = await streams.get('/streams');
-    dispatch ( { type: FETCH_STREAMS, payload: response.data } )
+    dispatch ({type: FETCH_STREAMS, payload: response.data});
 }
 
 export const fetchStream = id => async dispatch => {
